18152340db54b5faa7c0e1475361200bd7d7cee4
thadd-simpson-unison <thadd.simpson@unison.com>
2025-10-22 21:37:16 -0500

    feat: added config and home files

---
+++ configuration.nix
@@ -0,0 +1,80 @@
+# Nix Setup
+
+# Useful Commands
+# sudo nano /etc/nixos/configuration.nix
+# sudo nixos-rebuild switch
+
+# Imports
+{ config, pkgs, ... }:
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./home.nix
+    ];
+  
+  # Customizations
+  services.xserver.enable = true;
+  services.xserver.windowManager.i3.enable = true;
+  programs.bash.enable = true;
+  
+  # Bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  networking.hostName = "nixos"; # Define your hostname.  
+  #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.ts = {
+    isNormalUser = true;
+    description = "ts";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile.
+  environment.systemPackages = with pkgs; [
+    google-chrome
+    vscodium
+    arandr
+    git
+    gh
+    dbeaver-bin
+    slack
+    powershell
+    nodejs_22
+  ];
+
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+}
---
+++ home.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, ... }:
+let
+      home-manager = builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz;
+in
+{
+    imports = [
+        (import "${home-manager}/nixos")
+    ];
+
+    home-manager.useGlobalPkgs = true;
+    home-manager.users.ts = {
+        # The state version is required and should stay at the version you
+        # originally installed.
+        home.stateVersion = "25.05";
+
+        # Dotfiles
+        programs.git = {
+            enable = true;
+            userName = "thadd-simpson-unison";
+            userEmail = "thadd.simpson@unison.com"
+        }
+
+        home.xresources.properties = {
+            "XTerm*faceName" = "DejaVu Sans Mono";
+            "XTerm*background" = "#000000";
+            "XTerm*foreground" = "#8ABEB7";
+        }
+    };
+}
